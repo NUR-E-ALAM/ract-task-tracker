69abec7e669872777c4f663ca36a145df587ae12
Nur-E-Alam <tanzirnur@gmail.com>
2022-07-14 13:25:03 +0600

    working

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import { useState } from "react";
 import Header from "./components/Header";
 import Tasks from "./components/Tasks";
+import AddTask from "./AddTask";
 const App = () => {
 
-  const [tasks, onDelete] =  useState(
+  const [showAddTask, setShowAddTask] =  useState(false)
+
+  const [tasks, setTasks, onDelete] =  useState(
     [
         {
             id: 1,
@@ -32,16 +35,35 @@ const App = () => {
     ]
 )
 
+// Add Task
+const addTask = (task) => {
+  // console.log(task)
+
+  const id = Math.floor(Math.random()*1000) + 1
+  const newTask = { id, ...task }
+  setTasks([...tasks, newTask])
+}
+
+
 //Delete Task
 const deleteTask = (id) => {
-  console.log('Delete', id)
+  setTasks(tasks.filter((task) => task.id !== id))
+}
+
+//Toggle Reminder
+const toggoleReminder = (id) => {
+  // console.log(id)
+  setTasks(tasks.map((task) => task.id === id ? {...task, reminder: !task.reminder } : task))
 }
 
 
   return (
     <div className="container">
-<Header/>
-<Tasks tasks={tasks} onDelete={deleteTask} />
+<Header 
+onAdd={() => setShowAddTask(!showAddTask)} 
+showAdd={showAddTask} />
+{ showAddTask && <AddTask onAdd={addTask} />}
+{tasks.length > 0 ? <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggoleReminder} /> : 'No Tasks to Show'}
     </div>
   );
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,8 @@
 import PropTypes from 'prop-types'
 import Button from './Button'
- const Header = ({ title }) => {
+
+
+ const Header = ({ title, onAdd, showAdd }) => {
  const onClick = () => {
     console.log('Click')
  }
@@ -8,7 +10,10 @@ import Button from './Button'
   return (
     <header className='header'>
         <h1 style={headingStyle}>{title}</h1>
-       <Button color='green' text='Add' onClick={onClick} />
+       <Button 
+       color={showAdd ? 'red' : 'green'} 
+       text={showAdd ? 'Close' : 'Add'} 
+       onClick={onAdd} />
 
         </header>
   )
